e61c0bd20afcef8baa06921ca5dad0b340dacda0
user <user@192.168.0.4>
2023-04-04 06:12:19 -0700

    new

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,32 +1,32 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '@base/components/Header'
 import Footer from '@base/components/Footer'
 import FilterSearch from '@base/components/FilterSearch'
 import Head from 'next/head'
 
 function Search() {
-    const [theme, setTheme] = useState(false)
-    useEffect(() => {
-      theme ? document.body.classList.add("dark", "bg-black") : document.body.classList.remove("dark", "bg-black");
+  const [theme, setTheme] = useState(false)
+  useEffect(() => {
+    theme ? document.body.classList.add("dark", "bg-black") : document.body.classList.remove("dark", "bg-black");
   })
   return (
     <>
-     <Head>
-        <title>New Project</title>
-        <meta name="description" content="A blank Tailwind template" />
+      <Head>
+        <title>Search Page</title>
+        <meta name="description" content="Search Results" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <Header theme={theme} setTheme={setTheme} />
-      <div className='w-full bg-blue-100 h-[50px]'></div>
-      <div className='px-[100px] flex '>
-        <FilterSearch/>
+        <Header theme={theme} setTheme={setTheme} />
+        <div className='w-full bg-blue-100 h-[50px]'></div>
+        <div className='px-[100px] flex '>
+          <FilterSearch />
 
-      </div>
-      <Footer/>
+        </div>
+        <Footer />
       </main>
-      
+
     </>
   )
 }
