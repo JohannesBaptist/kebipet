4d64f8b844bab41bd5729ccbe325e48b258e255c
user <user@192.168.0.4>
2023-04-06 07:37:22 -0700

    Favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home({setting}: any) {
         <title>KebiPets - Homepage</title>
         <meta name="description" content="Your Favourite Pet Palace" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <main>
         
